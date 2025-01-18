e473b6d290fc84841ee1cb139a4de19ca195bb0c
Iman Zafar <85952424+imanzaf@users.noreply.github.com>
2025-01-18 18:58:21 +0000

    fixes

--- src/utils/data_models.py
+++ src/utils/data_models.py
@@ -13,9 +13,9 @@ from pydantic import BaseModel, Field
 class RegexPatterns(Enum):
     SCOPE1 = r"\b(scope\s*1|scope\s*i|scope\s*one)\b"
     SCOPE2 = r"\b(scope\s*2|scope\s*ii|scope\s*two)\b"
-    YEAR_1 = r"\b(?:FY|FISCAL\s*YEAR)?\s*(\d{2}|20\d{2})"
-    YEAR_2 = r"(?:[-–]\d{2}|\s)?\b"
-    UNITS_1 = r"(?:\b(?:\d+(?:,\d{3})*(?:\.\d+)?|million|billion|thousand)?)"
+    YEAR_1 = r"\b(?:FY|FISCAL\s*YEAR)?\s*(\d{2}|20\d{2}|[-–]\d{2})"
+    YEAR_2 = r"\b(\d{2}|20\d{2}|[-–]\d{2})"
+    UNITS_1 = r"\b(?:\d+(?:,\d{3})*(?:\.\d+)?|million|billion|thousand)"
     UNITS_2 = r"\s*(?:tco2e|tco2-e|co2e|co₂e|co2-eq|co₂-eq|mtco2e|mtco₂e|ktco2e|ktco₂e|mt|kt|tons?|tonnes?|metric\s*tons?)"
 
 
